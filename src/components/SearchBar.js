62cd0f328f71f6ec33542030504652702bb934d5
Austin Nash <nash13801@gmail.com>
2024-10-14 17:37:50 -0400

    fixed the enter input button

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,16 +8,23 @@ function SearchBar({onSearch}){
     }
 
     const search = useCallback(() =>{
-
+        
         onSearch(searchSong)
     }, [onSearch, searchSong]
     );
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault(); // Prevent default form submission
+            search(); // Trigger the search on Enter
+        }
+    };
 
     return(
         <div className = {styles.search}>
             <form className = {styles.form} >
                 <div className = {styles.bar}>
-                    <input type = "text" value = {searchSong} onChange = {handleChange} className = {styles.inputBar} size = '30' placeholder='Search a song'/>
+                    <input type = "text" value = {searchSong} onChange = {handleChange} className = {styles.inputBar} size = '30' placeholder='Search a song' onKeyDown={handleKeyDown}/>
                     <button className ={styles.button} type = "button" onClick = {search}>Search</button>
                 </div>
             </form>
